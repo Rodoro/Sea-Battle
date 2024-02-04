9f28e30a6cf76cd8e2e321973842e26ded5ae769
Lahtikov <134804238+Lahtikov@users.noreply.github.com>
2024-02-04 13:45:25 +0300

    Add files via upload

--- __test__/Modal.test.js
+++ __test__/Modal.test.js
@@ -1,1 +1,30 @@
+import '@testing-library/jest-dom'
+import { render } from '@testing-library/react';
+import Modal from '../src/components/interface/Modal';
+import { fireEvent } from '@testing-library/react';
 
+test('renders modal when visible is true', () => {
+    const { container } = render(<Modal visible={true} setVisible={() => {}} />);
+    const modal = container.querySelector('.visible');
+
+    expect(modal).toBeInTheDocument();
+});
+
+
+test('hides modal when visible is false', () => {
+    const { container } = render(<Modal visible={false} setVisible={() => {}} />);
+    const modal = container.querySelector('.invisible');
+
+    expect(modal).toBeInTheDocument();
+});
+
+
+test('calls setVisible function when clicking outside the modal', () => {
+    const setVisibleMock = jest.fn();
+    const { container } = render(<Modal visible={true} setVisible={setVisibleMock} />);
+    const modalOverlay = container.querySelector('.flex');
+
+    fireEvent.click(modalOverlay);
+
+    expect(setVisibleMock).toHaveBeenCalled();
+});
